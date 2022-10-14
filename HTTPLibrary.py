53e2a4668f66101f34451e8cb443e55ab99bc2cd
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2022-10-13 20:21:25 -0400

    added bonus parts

--- HTTPLibrary.py
+++ HTTPLibrary.py
@@ -1,3 +1,4 @@
+from email.header import Header
 import socket
 
 
@@ -18,11 +19,12 @@ class HTTPLibrary:
         BODY_DATA
         HEADERS: An array of strings formatted as 'k:v'. Example: ['"Content-Length": "17"', '"User-Agent": "Concordia-HTTP/1.0"']
     '''
-    def sendHTTPRequest(self, HOST, HTTP_METHOD, PATH = "/", QUERY_PARAMS = "", HEADERS = [], BODY_DATA = None, VERBOSE = False):
+    def sendHTTPRequest(self, HOST, HTTP_METHOD, PATH = "/", QUERY_PARAMS = "", HEADERS = [], BODY_DATA = None, VERBOSE = False, OUTPUT_FILE = None):
         # What is path is ""?
         # Merge query parameters and path?
         # input data other than json?
         # what if response is empty?
+        # re-directional url parse to domain?
         
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as TCPSocket:
             
@@ -32,10 +34,26 @@ class HTTPLibrary:
             TCPSocket.sendall(request)
             responseHeader, responseBody = self.__receiveResponse(TCPSocket)
 
-            if VERBOSE:
-                print(responseHeader)
-            
-            print(responseBody)        
+            if (self.__responseHeaderContainsRedirection(responseHeader)):
+                redirectionDomain = self.__findRedirectionDomain(responseHeader)
+
+                if redirectionDomain == "":
+                    print("Received 302 response code but didn't find the redirection URL")
+                    return 
+
+                self.sendHTTPRequestself(redirectionDomain, HTTP_METHOD, PATH, QUERY_PARAMS, HEADERS, BODY_DATA, VERBOSE, OUTPUT_FILE)
+
+            else:
+                if OUTPUT_FILE is not None:
+                    file = open(OUTPUT_FILE, "w")
+                    file.write(responseBody)
+                    file.close()
+                
+                if VERBOSE:
+                    print(responseHeader)
+                
+                if OUTPUT_FILE is None:
+                    print(responseBody)        
 
 
     '''
@@ -87,4 +105,19 @@ class HTTPLibrary:
         responseHeader, responseBody = response.split('\r\n\r\n', 1)
 
         return responseHeader, responseBody
+
+
+    def __responseHeaderContainsRedirection(self, responseHeaderString):
+        HEADERS = responseHeaderString.split('\r\n')
+        return '302' in HEADERS[0]
+
+
+    def __findRedirectionDomain(self, responseHeaderString):
+        HEADERS = responseHeaderString.split('\r\n')
+
+        for HEADER in HEADERS:
+            if 'location' in HEADER.lower():
+                key, value = HEADER.split(':')
+                return value.strip()
         
+        return ""
