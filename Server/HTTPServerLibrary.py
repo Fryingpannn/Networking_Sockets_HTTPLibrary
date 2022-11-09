e71f5927cb927843c8cf477ab50a027e1303b4b3
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2022-11-08 19:37:41 -0500

    converted HTTPServerLibrary to class

--- Server/HTTPServerLibrary.py
+++ Server/HTTPServerLibrary.py
@@ -1,26 +1,32 @@
 import socket
-import fileHandler
+from FileHandler import FileHandler
 
 '''
     PORT:       Integer     > Port to connect to
     DIRECTORY:  String      > Directory to use
     VERBOSE:    Boolean     > Print debugging information 
 '''
-def startServer(PORT, DIRECTORY, VERBOSE):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-        server_socket.bind(('localhost', PORT))
-        server_socket.listen(1)
+class HTTPServerLibrary:
+    def startServer(PORT, DIRECTORY, VERBOSE):
 
-        while True:    
-            # Wait for client connections
-            client_connection, client_address = server_socket.accept()
+        fileHandler = FileHandler()
+        fileHandler.setDefaultDirectory(DIRECTORY)
 
-            # Get the client request
-            request = client_connection.recv(1024).decode()
-            print(request)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-            # Send HTTP response
-            response = 'HTTP/1.0 200 OK\n\nHello World'
-            client_connection.sendall(response.encode())
-            client_connection.close()
+            server_socket.bind(('localhost', PORT))
+            server_socket.listen(1)
+
+            while True:    
+                # Wait for client connections
+                client_connection, client_address = server_socket.accept()
+
+                # Get the client request
+                request = client_connection.recv(1024).decode()
+                print(request)
+
+                # Send HTTP response
+                response = 'HTTP/1.0 200 OK\n\nHello World'
+                client_connection.sendall(response.encode())
+                client_connection.close()
