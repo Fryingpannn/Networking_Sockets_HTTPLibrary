0365b4e672f3be2e59ceb6a80414d24a7c360407
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2022-11-08 19:35:32 -0500

    added default Directory code

--- Server/FileHandler.py
+++ Server/FileHandler.py
@@ -11,23 +11,42 @@
     }
 '''
 
+import os
+import shutil
+from pathlib import Path
 
-'''
-    Possbile status code returned: 200
-'''
-def getNamesOfAllFiles():
-    pass
+class FileHandler:
+
+    def __init__(self):
+        self.defaultDirectory = 'Data'
+
+    def setDefaultDirectory(self, dirName):
+        if (dirName is not None):
+            self.defaultDirectory = dirName
+        
+        absolutePath = os.path.join(Path().resolve, self.defaultDirectory)
+
+        if Path(absolutePath).exists() and Path(absolutePath).is_dir():
+            shutil.rmtree(absolutePath)
+
+        Path(absolutePath).mkdir(parents=True)
+
+    '''
+        Possbile status code returned: 200
+    '''
+    def getNamesOfAllFiles():
+        pass
 
 
-'''
-    Possbile status code returned: 200, 400, 404
-'''
-def getFileContent(filename):
-    pass
+    '''
+        Possbile status code returned: 200, 400, 404
+    '''
+    def getFileContent(filename):
+        pass
 
 
-'''
-    Possbile status code returned: 200, 400
-'''
-def writeToFile(filename, filecontent):
-    pass
+    '''
+        Possbile status code returned: 200, 400
+    '''
+    def writeToFile(filename, filecontent):
+        pass
--- Server/HTTPServerLibrary.py
+++ Server/HTTPServerLibrary.py
@@ -1,34 +1,26 @@
-"""
- Implements a simple HTTP/1.0 Server
-
-"""
-
 import socket
+import fileHandler
 
+'''
+    PORT:       Integer     > Port to connect to
+    DIRECTORY:  String      > Directory to use
+    VERBOSE:    Boolean     > Print debugging information 
+'''
+def startServer(PORT, DIRECTORY, VERBOSE):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
 
-# Define socket host and port
-SERVER_HOST = 'localhost'
-SERVER_PORT = 8000
+        server_socket.bind(('localhost', PORT))
+        server_socket.listen(1)
 
-# Create socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-server_socket.listen(1)
-print('Listening on port %s ...' % SERVER_PORT)
+        while True:    
+            # Wait for client connections
+            client_connection, client_address = server_socket.accept()
 
-while True:    
-    # Wait for client connections
-    client_connection, client_address = server_socket.accept()
+            # Get the client request
+            request = client_connection.recv(1024).decode()
+            print(request)
 
-    # Get the client request
-    request = client_connection.recv(1024).decode()
-    print(request)
-
-    # Send HTTP response
-    response = 'HTTP/1.0 200 OK\n\nHello World'
-    client_connection.sendall(response.encode())
-    client_connection.close()
-
-# Close socket
-server_socket.close()
+            # Send HTTP response
+            response = 'HTTP/1.0 200 OK\n\nHello World'
+            client_connection.sendall(response.encode())
+            client_connection.close()
